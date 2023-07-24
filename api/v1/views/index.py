13affdd7140da7707697b29caaf733b9a22dc148
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-07-24 12:56:51 -0700

    Update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,19 +1,46 @@
 #!/usr/bin/python3
 """
-Comments
+index
 """
 
-# api/v1/views/index.py
-
+from flask import jsonify
 from api.v1.views import app_views
 
+from models import storage
 
-@app_views.route('/status', methods=['GET'])
-def get_status():
-    return {"status": "OK"}
 
-    counts = {
-        "your_object_type": storage.count("your_object_type"),
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
     }
 
-    return jsonify(counts)
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
